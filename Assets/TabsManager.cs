37175fb9fa0cdf582b496df109660e9ad86c4207
brentli1 <brent.jlinville@gmail.com>
2017-01-28 10:28:18 -0600

    Initial commit

---
+++ Assets/TabsManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TabsManager : MonoBehaviour {
+
+	public List<GameObject> tabsMenuButtons = new List<GameObject>();
+	public List<GameObject> tabsContainers = new List<GameObject>();
+
+	void Start() {
+		CloseAllTabs ();
+		tabsContainers [0].SetActive (true);
+		SetAllTabItems (tabsMenuButtons [0].GetComponent<TabsMenuItem> ());
+	}
+
+	public void ToggleTabs(GameObject tab, TabsMenuItem menuItem) {
+		CloseAllTabs ();
+		SetAllTabItems (menuItem);
+		tab.SetActive (true);
+	}
+
+	private void CloseAllTabs() {
+		foreach (GameObject tabs in tabsContainers) {
+			tabs.SetActive(false);
+		}
+	}
+
+	private void SetAllTabItems(TabsMenuItem menuItem) {
+		foreach (GameObject item in tabsMenuButtons) {
+			item.GetComponent<TabsMenuItem>().isActive = false;
+			item.GetComponent<TabsMenuItem>().SetMenuItemState ();
+		}
+
+		menuItem.isActive = true;
+		menuItem.SetMenuItemState ();
+	}
+}
---
+++ Assets/TabsMenuItem.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TabsMenuItem : MonoBehaviour {
+
+	public GameObject tab;
+	public bool isActive;
+
+	private TabsManager TM;
+	private Text thisText;
+	private Image thisImage;
+	private Color highlightColor = new Color(1,1,1,1);
+	private Color dehighlightColor = new Color(72f/255f, 76f/255f, 85f/255f,1);
+
+	public void Awake() {
+		TM = GameObject.Find ("TabsManager").GetComponent<TabsManager> ();
+		thisText = transform.Find ("Text").GetComponent<Text> ();
+		thisImage = GetComponent<Image> ();
+	}
+
+	public void ClickTabItem() {
+		TM.ToggleTabs (tab, this);
+	}
+
+	public void SetMenuItemState() {
+		if (isActive) {
+			thisImage.color = highlightColor;
+			thisText.color = dehighlightColor;
+		} else {
+			thisImage.color = dehighlightColor;
+			thisText.color = highlightColor;
+		}
+	}
+}
